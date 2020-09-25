8314e5b0fd157e56ed0e7004d2a69be78e6c1933
Nicholas Gelinas <ngelinas@pyx4.com>
2020-09-24 21:08:00 -0400

    Re-exports types with dedicated `export type`
    To satisfy the Typescript compiler when used with `--isolated-modules`,
    this change re-exports types with dedicated `export type` statements.
    If the exports from those files also included actual value exports, a
    new `export type` statement was added directly below the existing export
    to collocate the value and type exports coming from the same URL.

--- mod.ts
+++ mod.ts
@@ -3,4 +3,5 @@ import { GraphQLError, gql, PubSub } from "./deps.ts"
 
 export { gql, PubSub }
 export const GQLError = GraphQLError as any;
-export { applyGraphQL, ApplyGraphQLOptions, ResolversProps } from "./applyGraphQL.ts";
+export { applyGraphQL } from "./applyGraphQL.ts";
+export type { ApplyGraphQLOptions, ResolversProps } from "./applyGraphQL.ts";
