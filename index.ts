c78c41ae007fce6eddc2f9a3fc7abdc47876aced
MarcoZeee <marcoshihh@gmail.com>
2023-07-31 13:25:37 +0800

    feat: oak version upgrade && docs modification for clearness

---
+++ index.ts
@@ -0,0 +1,92 @@
+import { Application, Router } from "https://deno.land/x/oak/mod.ts";
+import {
+  applyGraphQL,
+  gql,
+  GQLError,
+} from "https://deno.land/x/oak_graphql/mod.ts";
+
+const app = new Application();
+
+// Logger
+app.use(async (ctx, next) => {
+  await next();
+  const rt = ctx.response.headers.get("X-Response-Time");
+  console.log(`${ctx.request.method} ${ctx.request.url} - ${rt}`);
+});
+
+// Timing
+app.use(async (ctx, next) => {
+  const start = Date.now();
+  await next();
+  const ms = Date.now() - start;
+  ctx.response.headers.set("X-Response-Time", `${ms}ms`);
+});
+
+
+
+const types = gql`
+  type User {
+    firstName: String
+    lastName: String
+  }
+
+  input UserInput {
+    firstName: String
+    lastName: String
+  }
+
+  type ResolveType {
+    done: Boolean
+  }
+
+  type Query {
+    getUser(id: String): User
+  }
+
+  type Mutation {
+    setUser(input: UserInput!): ResolveType!
+  }
+`;
+
+const resolvers = {
+  Query: {
+    getUser: (parent: any, { id }: any, context: any, info: any) => {
+      console.log("id", id, context);
+      if (context.user === "Aaron") {
+        throw new GQLError({ type: "auth error in context" });
+      }
+      return {
+        firstName: "wooseok",
+        lastName: "lee",
+      };
+    },
+  },
+  Mutation: {
+    setUser: (
+      parent: any,
+      { input: { firstName, lastName } }: any,
+      context: any,
+      info: any
+    ) => {
+      console.log("input:", firstName, lastName);
+      return {
+        done: true,
+      };
+    },
+  },
+};
+
+const GraphQLService = await applyGraphQL<Router>({
+  Router,
+  typeDefs: types,
+  resolvers: resolvers,
+  context: (ctx) => {
+    // this line is for passing a user context for the auth
+    return { user: "Aaron" };
+  },
+});
+
+app.use(GraphQLService.routes(), GraphQLService.allowedMethods());
+
+console.log("Server start at http://localhost:8080");
+await app.listen({ port: 8080 });
